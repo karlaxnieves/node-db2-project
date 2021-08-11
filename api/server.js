f9b579be4033de5e7030e4b30e182dea24d8694b
Karla Nieves <karlaxnieves@gmail.com>
2021-08-11 09:12:15 -0700

    I am working on some errors

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const CarsRouter = require('./cars/cars-router.js')
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+
+server.use("/api/cars", CarsRouter)
+
+server.use('*', (req, res, next) => {
+    next({status: 404, message: 'not found'})
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 module.exports = server
